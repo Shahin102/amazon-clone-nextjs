b67af9141a7d1721d277d1b3c3a40fbb276ade2f
Shahin <shahinzaman102@gmail.com>
2021-06-15 02:09:22 +0600

    2nd time deploy

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAeyDcszlv5Nt7uxQjsSleZ1IpNWpBdILE",
+    authDomain: "nextjsapp-69952.firebaseapp.com",
+    projectId: "nextjsapp-69952",
+    storageBucket: "nextjsapp-69952.appspot.com",
+    messagingSenderId: "313921451391",
+    appId: "1:313921451391:web:9a7ba86772c7e64bf1d64a",
+    measurementId: "G-9CSXM54GXJ"
+  };
+
+  
